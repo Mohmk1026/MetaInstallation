91e67eba1e11e1c8a032ac3561adb7fc2ac200d9
Mohmk1026 <96932279+Mohmk1026@users.noreply.github.com>
2022-01-14 10:37:09 +0800

    Update update_config.sh

--- update_config.sh
+++ update_config.sh
@@ -2,12 +2,12 @@
 
 cd /usr/local/go/src/github.com/pandotoken/pando/integration/pandonet/node
 rm -rf config.yaml
-wget https://raw.githubusercontent.com/flame1833/MetaInstallation/master/config.yaml
+wget https://raw.githubusercontent.com/Mohmk1026/MetaInstallation/master/config.yaml
 chmod +x config.yaml
 
 cd /usr/local/go/src/github.com/pandotoken/pandonet/node
 rm -rf config.yaml
-wget https://raw.githubusercontent.com/flame1833/MetaInstallation/master/config.yaml
+wget https://raw.githubusercontent.com/Mohmk1026/MetaInstallation/master/config.yaml
 chmod +x config.yaml
 
 rm -rf /usr/local/go/src/github.com/pandotoken/pandonet/node/db/main/*
